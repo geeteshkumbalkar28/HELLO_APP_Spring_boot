1e76b0d96d09c2c3f93fe621927f6518ba4309e6
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-04-08 04:51:22 +0530

    Added : Make REST Call to show Hello Mark from BridgeLabz

--- src/main/java/com/bridgelabz/HelloApp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/HelloApp/controller/HelloController.java
@@ -2,11 +2,17 @@ package com.bridgelabz.HelloApp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class HelloController {
     @RequestMapping(value="/hello" , method = RequestMethod.GET)
     public String message(){
         return "Hello from BridgeLabz";
     }
+    @RequestMapping(value="/hello/query",method = RequestMethod.GET)
+    public String message1(@RequestParam String name){
+        return name;
+    }
+
 
 }
