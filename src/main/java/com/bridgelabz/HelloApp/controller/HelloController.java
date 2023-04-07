49e529f00a21a0c53073932660ce944d2071d652
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-04-08 04:49:41 +0530

    Added : Create a Rest Controller to demonstrate the various HTTP Methods and respond hello messages to the User.

--- src/main/java/com/bridgelabz/HelloApp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/HelloApp/controller/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 public class HelloController {
-    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    @RequestMapping(value="/hello" , method = RequestMethod.GET)
     public String message(){
         return "Hello from BridgeLabz";
     }
