a9982808686f494f2c3c1e834d518b7507906594
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-04-08 04:47:56 +0530

    Added : Create a Rest Controller to demonstrate

---
+++ src/main/java/com/bridgelabz/HelloApp/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.HelloApp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class HelloController {
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    public String message(){
+        return "Hello from BridgeLabz";
+    }
+
+}
